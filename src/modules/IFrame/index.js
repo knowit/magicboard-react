0c4b32800ad1503ae987c321846a8865d68750ef
petterlovereide <petter.lovereide@gmail.com>
2018-10-25 09:55:35 +0200

    Added IFrame

---
+++ src/modules/IFrame/index.js
@@ -0,0 +1,47 @@
+// @flow
+import React from 'react';
+import Iframe from 'react-iframe';
+
+type Props = {
+  url: string,
+};
+type State = {
+  frame: number,
+};
+
+class IFrame extends React.Component<Props, State> {
+  constructor() {
+    super();
+    this.state = {
+      frame: 0,
+    };
+  }
+
+  componentDidMount() {
+    this.intervalId = setInterval(this.tick, 1000 * 60 * 5);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  tick = () => {
+    this.setState(prevState => ({ frame: prevState.frame + 1 }));
+  };
+
+  render() {
+    return (
+      <Iframe
+        url={this.props.url}
+        width="100%"
+        height="100%"
+        id="myId"
+        display="initial"
+        position="relative"
+        allowFullScreen
+      />
+    );
+  }
+}
+
+export default IFrame;
