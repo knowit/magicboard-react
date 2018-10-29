033446720d644d18ac650748293641a42ede1dfb
petterlovereide <petter.lovereide@gmail.com>
2018-10-29 12:54:04 +0100

    Yr: Image loading and redesign

---
+++ src/modules/Yr/images.js
@@ -0,0 +1,65 @@
+// @flow
+
+import img01d from './img/01d.svg';
+import img01m from './img/01m.svg';
+import img01n from './img/01n.svg';
+import img02d from './img/02d.svg';
+import img02m from './img/02m.svg';
+import img02n from './img/02n.svg';
+import img03d from './img/03d.svg';
+import img03m from './img/03m.svg';
+import img03n from './img/03n.svg';
+import img4 from './img/04.svg';
+import img05d from './img/05d.svg';
+import img05m from './img/05m.svg';
+import img05n from './img/05n.svg';
+import img06d from './img/06d.svg';
+import img06m from './img/06m.svg';
+import img06n from './img/06n.svg';
+import img07d from './img/07d.svg';
+import img07m from './img/07m.svg';
+import img07n from './img/07n.svg';
+import img08d from './img/08d.svg';
+import img08m from './img/08m.svg';
+import img08n from './img/08n.svg';
+import img09 from './img/09.svg';
+import img10 from './img/10.svg';
+import img11 from './img/11.svg';
+import img12 from './img/12.svg';
+import img13 from './img/13.svg';
+import img14 from './img/14.svg';
+import img15 from './img/15.svg';
+
+
+export const images = {
+  "1d": img01d,
+  "1m": img01m,
+  "1n": img01n,
+  "2d": img02d,
+  "2m": img02m,
+  "2n": img02n,
+  "3d": img03d,
+  "3m": img03m,
+  "3n": img03n,
+  "4": img4,
+  "5d": img05d,
+  "5m": img05m,
+  "5n": img05n,
+  "6d": img06d,
+  "6m": img06m,
+  "6n": img06n,
+  "7d": img07d,
+  "7m": img07m,
+  "7n": img07n,
+  "8d": img08d,
+  "8m": img08m,
+  "8n": img08n,
+  "9": img09,
+  "10": img10,
+  "11": img11,
+  "12": img12,
+  "13": img13,
+  "14": img14,
+  "15": img15,
+
+};
