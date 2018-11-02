3ac43a890b1ef7f6c823d892476ccafcb7f31bd5
Kien Vu <kienv91@gmail.com>
2018-11-02 17:18:39 +0100

    removed switch mode button, set in project instead

--- client/src/containers/Caroussel/index.js
+++ client/src/containers/Caroussel/index.js
@@ -6,14 +6,10 @@ import { onMount, onUnmount } from 'react-keydown/dist/event_handlers';
 import { setBinding } from 'react-keydown/dist/store';
 import { boards } from '../../App';
 
-import { ButtonContainer, Button } from './components';
-
 import type { Action } from '../../actions';
 
 type Props = {
   slideIndex: number,
-  viewMode: 'landscape' | 'portrait',
-  switchViewMode: () => void,
   nextSlide: () => void,
   previousSlide: () => void,
 };
@@ -37,24 +33,8 @@ class Caroussel extends React.PureComponent<Props> {
     }
   }
 
-  switchMode = () => {
-    console.log('switched');
-    this.props.switchViewMode();
-  };
-
   render() {
-    return (
-      <React.Fragment>
-        <ButtonContainer>
-          <Button type="button" onClick={this.switchMode}>
-            switch view mode
-          </Button>
-        </ButtonContainer>
-        <Carousel slideIndex={this.props.slideIndex}>
-          {boards(this.props.viewMode)}
-        </Carousel>
-      </React.Fragment>
-    );
+    return <Carousel slideIndex={this.props.slideIndex}>{boards()}</Carousel>;
   }
 }
 
@@ -66,13 +46,11 @@ setBinding({
 
 const ConnectedApp = connect(
   state => ({
-    viewMode: state.rootReducer.viewMode,
     slideIndex: state.rootReducer.slideIndex,
   }),
   (dispatch: (action: Action) => void) => ({
     nextSlide: () => dispatch({ type: 'NEXT_BOARD' }),
     previousSlide: () => dispatch({ type: 'PREVIOUS_BOARD' }),
-    switchViewMode: () => dispatch({ type: 'SWITCH_VIEW_MODE' }),
   }),
 )(Caroussel);
 
