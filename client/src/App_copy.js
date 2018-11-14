38cdb50572c35aae02d3d5a29abe06468ae6c108
petterlovereide <petter.lovereide@gmail.com>
2018-11-14 14:41:27 +0100

    version 1.0

--- client/src/App_copy.js
+++ client/src/App_copy.js
@@ -4,35 +4,22 @@ import uuidv4 from 'uuid/v4';
 
 import { Grid, Cell } from './containers';
 import Ruter from './modules/Ruter';
-import Sundtcommander from './modules/Sundtcommander';
 import Clock from './modules/Clock';
 
 const ROW_CELLS = 5;
 const COLUMN_CELLS = 10;
 
-export const boards = (viewMode?: 'landscape' | 'portrait') => {
-  const row = `repeat(${
-    viewMode === 'landscape' ? ROW_CELLS : COLUMN_CELLS
-  }, 1fr)`;
-  const column = `repeat(${
-    viewMode === 'landscape' ? COLUMN_CELLS : ROW_CELLS
-  }, 1fr)`;
-
-  return [
-    <Grid key={uuidv4()} row={row} column={column}>
-      <Clock />
-      <Sundtcommander />
-      <Ruter />
-      <Cell />
-      <Cell />
-      <Cell />
-      <Cell />
-    </Grid>,
-    <Grid key={uuidv4()} row={row} column={column}>
-      <Cell />
-      <Cell />
-      <Ruter />
-    </Grid>,
-  ];
-};
-
+export const boards = () => [
+  <Grid key={uuidv4()} row={ROW_CELLS} column={COLUMN_CELLS}>
+    <Clock />
+    <Cell />
+    <Cell />
+    <Cell />
+    <Cell />
+  </Grid>,
+  <Grid key={uuidv4()} row={ROW_CELLS} column={COLUMN_CELLS}>
+    <Cell />
+    <Cell />
+    <Ruter />
+  </Grid>,
+];
---
+++ client/src/modules/RTGoogleAnalytics/README.md
@@ -0,0 +1,9 @@
+# IFrame
+
+
+## Using the module
+
+To use this module, add the following configuration in the App.js file:
+```
+<RTGoogleAnalytics viewId="" name=""/>
+```
