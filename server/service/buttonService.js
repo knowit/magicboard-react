3bfd33d38138a45027fa45847af42e53f217a5ac
petterlovereide <petter.lovereide@gmail.com>
2018-11-16 16:31:45 +0100

    serialport buttons fix

--- server/service/buttonService.js
+++ server/service/buttonService.js
@@ -5,7 +5,7 @@ const port = new SerialPort("COM4", { baudRate: 9600 });
 const parser = port.pipe(new Readline({ delimiter: "\n" }));
 
 function buttonService(ws, req) {
-  parser.on("data", data => ws.send(data));
+  parser.on("data", data => ws.send(data.toString('utf-8')));
 }
 
 module.exports = buttonService;
