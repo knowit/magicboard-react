32c8b7aad8fc3620de755587c32e2ddcca1f4029
Bjornar Dalsnes <brd@Bjornars-MacBook-Pro.local>
2018-11-19 12:24:55 +0100

    Added status diagram

--- client/src/modules/Jira/services.js
+++ client/src/modules/Jira/services.js
@@ -1,8 +1,9 @@
 // @flow
 import moment from 'moment';
-import { Mode } from './types';
+import { Mode, Issue } from './types';
+import { dataObject } from './options';
 
-const AUTH = 'Basic c2FsZy1zdGF0aXN0aWtrOnJnRFU3NVptWlJXbkRkRg=='; // Access to https://support.knowit.no, user: salg-statistikk
+const AUTH = ''; // Access to https://support.knowit.no, user: salg-statistikk
 const PROXYURL = 'https://aqueous-oasis-75157.herokuapp.com/'; // Owned by bjornar.dalsnes@knowit.no
 
 const MONTHS = [
@@ -21,8 +22,8 @@ const MONTHS = [
 ];
 
 // Excludes statuses "Rejected" and "Done" from the issues and issues from other years
-const filterIssues = (issues: ?(Object[]), year: number) => {
-  const filteredIssues: ?(Object[]) = [];
+const filterIssues = (issues: Issue[], year: number) => {
+  const filteredIssues: Issue[] = [];
 
   for (let i = 0; i < issues.length; i += 1) {
     const date = new Date(issues[i].fields.created);
@@ -58,7 +59,11 @@ const generateMonthData = (projectKey: string, year: number) =>
       const date = new Date(issues[i].fields.created);
       issuesPerMonth[date.getMonth()] += 1;
     }
-    return issuesPerMonth;
+
+    dataObject.label = `Antall anbud i ${year}`;
+    dataObject.data = issuesPerMonth;
+
+    return dataObject;
   });
 
 const generateWeekData = (projectKey: string, year: number) =>
@@ -69,7 +74,11 @@ const generateWeekData = (projectKey: string, year: number) =>
       const momentDate = moment(issues[i].fields.created); // moment has to be used to get week number
       issuesPerWeek[momentDate.week()] += 1;
     }
-    return issuesPerWeek;
+
+    dataObject.label = `Antall anbud i ${year}`;
+    dataObject.data = issuesPerWeek;
+
+    return dataObject;
   });
 
 const generateStatusData = (projectKey: string, year: number) =>
@@ -84,7 +93,11 @@ const generateStatusData = (projectKey: string, year: number) =>
       }
       issuesPerStatus[xLabels.indexOf(issues[i].fields.status.name)] += 1;
     }
-    return { issuesPerStatus, xLabels };
+
+    dataObject.label = `Antall anbud i ${year}`;
+    dataObject.data = issuesPerStatus;
+
+    return { dataObject, xLabels };
   });
 
 export const generateChartData = (
@@ -92,68 +105,26 @@ export const generateChartData = (
   year: number,
   mode: Mode,
 ) => {
-  let issuesPromise;
-  let xLabels;
-
-  if (mode === Mode.WEEK) {
-    issuesPromise = generateWeekData(projectKey, year);
-    xLabels = [...Array(54).keys()].slice(1); // [1...53], week numbers
-  } else if (mode === Mode.MONTH) {
-    issuesPromise = generateMonthData(projectKey, year);
-    xLabels = MONTHS;
-  } else if (mode === Mode.STATUS) {
-    issuesPromise = generateStatusData(projectKey, year);
+  if (mode === Mode.MONTH) {
+    const issuesTimePromise = generateMonthData(projectKey, year);
+    return issuesTimePromise.then(issueData => ({
+      labels: MONTHS,
+      datasets: [issueData],
+    }));
   }
-
-  return issuesPromise.then(issueCount => {
-    const data = {
-      labels: mode !== Mode.STATUS ? xLabels : issueCount.xLabels,
-      datasets: [
-        {
-          label:
-            mode !== Mode.STATUS
-              ? `Antall anbud i ${year}`
-              : `Status på anbud i ${year}`,
-          backgroundColor: 'rgba(65, 149, 165, 0.2)',
-          borderColor: 'rgb(65, 149, 165)',
-          borderWidth: 1,
-          hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-          hoverBorderColor: 'rgba(255,99,132,1)',
-          data: mode !== Mode.STATUS ? issueCount : issueCount.issuesPerStatus,
-        },
-      ],
-    };
-
-    const options = {
-      maintainAspectRatio: false,
-      scales: {
-        xAxes: [
-          {
-            display: true,
-            ticks: {
-              fontSize: 14,
-              fontColor: 'white',
-            },
-          },
-        ],
-        yAxes: [
-          {
-            display: true,
-            ticks: {
-              fontSize: 20,
-              fontColor: 'white',
-            },
-          },
-        ],
-      },
-      legend: {
-        labels: {
-          // This more specific font property overrides the global property
-          fontColor: 'white',
-          fontSize: 18,
-        },
-      },
-    };
-    return { data, options };
-  });
+  if (mode === Mode.WEEK) {
+    const issuesTimePromise = generateWeekData(projectKey, year);
+    return issuesTimePromise.then(issueData => ({
+      labels: [...Array(54).keys()].slice(1).map(String), // [1...53], week numbers
+      datasets: [issueData],
+    }));
+  }
+  if (mode === Mode.STATUS) {
+    const issuesStatusPromise = generateStatusData(projectKey, year);
+    return issuesStatusPromise.then(issueData => ({
+      labels: issueData.xLabels,
+      datasets: [issueData.dataObject],
+    }));
+  }
+  return null;
 };
