5217c73ee0877f849fd2bff27226c12af525bf25
Maximusnor <espenbf@ifi.uio.no>
2018-11-21 12:44:31 +0100

    Added support for google assistant communication

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
-const express = require("express");
-const app = express();
-const expressWs = require("express-ws")(app);
+var express = require('express');
+var expressWs = require('express-ws');
+var expressWs = expressWs(express());
+var app = expressWs.app;
 
 const motionService = require("./service/motionService");
 const buttonService = require("./service/buttonService");
 const PORT = 4000;
+
+app.use(express.static('public'));
+var aWss = expressWs.getWss('/');
+
 
 app.get("/", (req, res) => {
   res.send("server is running");
@@ -12,5 +17,17 @@ app.get("/", (req, res) => {
 
 app.ws("/motion", motionService);
 app.ws("/button", buttonService);
+
+
+app.ws('/googleAssistant', function(ws, req) {
+    console.log('Socket Connected');
+
+    ws.onmessage = function(msg) {
+        console.log(msg.data);
+        aWss.clients.forEach(function (client) {
+            client.send(msg.data);
+        });
+    };
+});
 
 app.listen(PORT);
