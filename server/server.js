75feb61f283b1aaf6822447bf2b1b40f2063a832
Maximusnor <espenbf@ifi.uio.no>
2018-11-21 15:45:27 +0100

    Fixed bugs on web sockets closing

--- server/server.js
+++ server/server.js
@@ -1,33 +1,22 @@
-var express = require('express');
-var expressWs = require('express-ws');
-var expressWs = expressWs(express());
-var app = expressWs.app;
+const express = require('express');
+let expressWs = require('express-ws');
+expressWs = expressWs(express());
+const app = expressWs.app;
 
 const motionService = require("./service/motionService");
 const buttonService = require("./service/buttonService");
+const googleAssistantService = require("./service/googleAssistantService");
 const PORT = 4000;
 
-app.use(express.static('public'));
-var aWss = expressWs.getWss('/');
+//app.use(express.static('public'));
 
 
 app.get("/", (req, res) => {
-  res.send("server is running");
+    res.send("server is running");
 });
 
 app.ws("/motion", motionService);
 app.ws("/button", buttonService);
-
-
-app.ws('/googleAssistant', function(ws, req) {
-    console.log('Socket Connected');
-
-    ws.onmessage = function(msg) {
-        console.log(msg.data);
-        aWss.clients.forEach(function (client) {
-            client.send(msg.data);
-        });
-    };
-});
+app.ws('/googleAssistant', googleAssistantService);
 
 app.listen(PORT);
