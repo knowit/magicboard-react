17a85a43b5bffe74a5b7895ed9bd72a3f3440371
Maximusnor <espenbf@ifi.uio.no>
2018-11-21 15:45:27 +0100

    Fixed bugs on web sockets closing

--- server/service/motionService.js
+++ server/service/motionService.js
@@ -1,11 +1,23 @@
 const SerialPort = require("serialport");
 const Readline = require("@serialport/parser-readline");
 
-const port = new SerialPort("COM3", { baudRate: 9600 });
+const port = new SerialPort("COM8", { baudRate: 9600 });
 const parser = port.pipe(new Readline({ delimiter: "\n" }));
 
 function motionService(ws, req) {
-  parser.on("data", data => ws.send(data));
-}
+    ws.on("message", msg => {
+        console.log(msg);
+        client = ws;
+    });
 
+    ws.on("close", () => {
+        client = null;
+    });
+
+    parser.on("data", data => {
+        if (client !== null) {
+            client.send(data)
+        }
+    });
+}
 module.exports = motionService;
