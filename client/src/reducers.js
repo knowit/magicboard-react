6dc7194cbdf67d29f06e0fcd3ce48cf755a59efb
Maximusnor <espenbf@ifi.uio.no>
2018-11-23 15:45:43 +0100

    Added more options for voice commands in Client

--- client/src/reducers.js
+++ client/src/reducers.js
@@ -7,50 +7,70 @@ import { boards } from './App';
 const NUMBER_OF_BOARDS = boards().length - 1;
 
 type ReducerState = {
-  noMotionDetected: boolean,
-  slideIndex: number,
-  googleAction: ?string,
+    noMotionDetected: boolean,
+    slideIndex: number,
+    googleAction: ?string,
 };
 
 const initialState = {
-  noMotionDetected: false,
-  slideIndex: NUMBER_OF_BOARDS === 0 ? NUMBER_OF_BOARDS : 1,
-  googleAction: undefined,
+    noMotionDetected: false,
+    slideIndex: NUMBER_OF_BOARDS === 0 ? NUMBER_OF_BOARDS : 1,
+    googleAction: undefined,
 };
 
 const rootReducer = (state: ReducerState = initialState, action: Action) => {
-  switch (action.type) {
-    case 'NEXT_BOARD':
-      return {
-        ...state,
-        slideIndex:
-          state.slideIndex < NUMBER_OF_BOARDS
-            ? state.slideIndex + 1
-            : state.slideIndex,
-      };
+    switch (action.type) {
+        case 'NEXT_BOARD':
+            return {
+                ...state,
+                slideIndex:
+                    state.slideIndex < NUMBER_OF_BOARDS
+                        ? state.slideIndex + 1
+                        : state.slideIndex,
+            };
 
-    case 'PREVIOUS_BOARD':
-      return {
-        ...state,
-        slideIndex:
-          state.slideIndex > 0 ? state.slideIndex - 1 : state.slideIndex,
-      };
+        case 'PREVIOUS_BOARD':
+            return {
+                ...state,
+                slideIndex:
+                    state.slideIndex > 0 ? state.slideIndex - 1 : state.slideIndex,
+            };
 
-    case 'MOTION_DETECTED':
-      return {
-        ...state,
-        noMotionDetected: false,
-      };
+        case 'GENERAL_INFO_BOARD':
+            return {
+                ...state,
+                slideIndex: 0,
+            };
 
-    case 'NO_MOTION_DETECTED':
-      return {
-        ...state,
-        noMotionDetected: true,
-      };
+        case 'BUILDING_INFO_BOARD':
+            return {
+                ...state,
+                slideIndex: 1,
+            };
 
-    default:
-      return state;
-  }
+        case 'SALES_INFO_BOARD':
+            return {
+                ...state,
+                slideIndex: 2,
+            };
+
+        case 'MOTION_DETECTED':
+            return {
+                ...state,
+                noMotionDetected: false,
+            };
+
+
+
+        case 'NO_MOTION_DETECTED':
+            return {
+                ...state,
+                noMotionDetected: true,
+            };
+
+        default:
+            return state;
+    }
 };
 
 export default combineReducers({ rootReducer });
