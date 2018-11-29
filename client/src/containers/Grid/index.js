03b848e896fae94b4bb751157faf3efa422bc9fd
Bjornar Dalsnes <brd@Bjornars-MacBook-Pro.local>
2018-11-29 09:59:32 +0100

    Added row and col props

--- client/src/containers/Grid/index.js
+++ client/src/containers/Grid/index.js
@@ -3,9 +3,10 @@ import React from 'react';
 import styled from 'react-emotion';
 
 type Props = {
-  column: string,
-  row: string,
-  nested?: boolean,
+    column: string,
+    row: string,
+    nested?: boolean,
+    horizontal?: boolean,
 };
 
 const StyledGrid = styled('div')`
@@ -14,6 +15,7 @@ const StyledGrid = styled('div')`
   grid-template-columns: ${props => props.column};
   grid-gap: 8px;
   overflow: hidden;
+  grid-auto-flow: ${props => (props.horizontal ? 'column' : 'row')};
   width: ${props => (props.nested ? '100%' : 'auto')};
   height: ${props => (props.nested ? '100%' : '100vh')};
 `;
--- client/src/modules/Jira/types.js
+++ client/src/modules/Jira/types.js
@@ -1,40 +1,41 @@
 export const Mode = Object.freeze({
-  MONTH: Symbol('month'),
-  WEEK: Symbol('week'),
-  STATUS: Symbol('status'),
+    MONTH: Symbol('month'),
+    WEEK: Symbol('week'),
+    STATUS: Symbol('status'),
 });
 export type Issue = {
-  expand: string,
-  id: string,
-  key: string,
-  self: string,
-  fields: {
-    created: string,
-    status: {
-      description: string,
-      iconUrl: string,
-      id: string,
-      name: string,
-      self: string,
-      statusCategory: {
-        colorName: string,
-        id: number,
-        key: string,
-        name: string,
-        self: string,
-      },
+    expand: string,
+    id: string,
+    key: string,
+    self: string,
+    fields: {
+        created: string,
+        status: {
+            description: string,
+            iconUrl: string,
+            id: string,
+            name: string,
+            self: string,
+            statusCategory: {
+                colorName: string,
+                id: number,
+                key: string,
+                name: string,
+                self: string,
+            },
+        },
     },
-  },
 };
 
 export type Props = {
-  projectKey: string, // Always SALG?
-  year: number,
-  modeArray: Mode[],
-  auth: string,
+    row?: string,
+    column?: string,
+    projectKey: string,
+    year: number,
+    modeArray: Mode[],
+    auth: string,
 };
 export type State = {
-  data: ?(Object[]),
-  modeIndex: number,
-  frame: number,
+    data: ?(Object[]),
+    modeIndex: number,
 };
