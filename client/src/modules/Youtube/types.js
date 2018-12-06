1867f91fa9e8b6afd6d978814c9c7e2556f48d7a
Bjornar Dalsnes <brd@Bjornars-MacBook-Pro.local>
2018-12-06 10:02:53 +0100

    Backend done

--- client/src/modules/Youtube/types.js
+++ client/src/modules/Youtube/types.js
@@ -1,3 +1,11 @@
+export type Video = {
+  title: string,
+  views: number,
+  likes: number,
+  description: string,
+  thumbnail: string,
+};
+
 export type Props = {
   row?: string,
   column?: string,
@@ -5,5 +13,7 @@ export type Props = {
 };
 
 export type State = {
-  data: ?(Object[]),
+  playlistName: string,
+  totalViews: number,
+  videos: Video[],
 };
