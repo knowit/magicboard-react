774ec73471724beda0f8e66972343b062a572ba3
Bjornar Dalsnes <brd@Bjornars-MacBook-Pro.local>
2018-12-06 12:30:42 +0100

    Videos sorted by views

--- client/src/modules/Youtube/types.js
+++ client/src/modules/Youtube/types.js
@@ -10,10 +10,12 @@ export type Props = {
   row?: string,
   column?: string,
   channelId: string,
+  maxVideos: number,
 };
 
 export type State = {
   playlistName: string,
   totalViews: number,
+  totalLikes: number,
   videos: Video[],
 };
