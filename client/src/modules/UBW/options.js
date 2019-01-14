b1988a54356e52564dd912a4eaf8521cf2269d2d
Bjornar Dalsnes <brd@Bjornars-MacBook-Pro.local>
2019-01-14 14:11:07 +0100

    working version

---
+++ client/src/modules/UBW/options.js
@@ -0,0 +1,40 @@
+export const options = {
+  maintainAspectRatio: false,
+  scales: {
+    xAxes: [
+      {
+        display: true,
+        ticks: {
+          fontSize: 14,
+          fontColor: 'white',
+        },
+      },
+    ],
+    yAxes: [
+      {
+        display: true,
+        ticks: {
+          fontSize: 20,
+          fontColor: 'white',
+        },
+      },
+    ],
+  },
+  legend: {
+    labels: {
+      // This more specific font property overrides the global property
+      fontColor: 'white',
+      fontSize: 18,
+    },
+  },
+};
+
+export const dataObject = {
+  // label: ,
+  // data: ,
+  backgroundColor: 'rgba(65, 149, 165, 0.2)',
+  borderColor: 'rgb(65, 149, 165)',
+  borderWidth: 1,
+  hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+  hoverBorderColor: 'rgba(255,99,132,1)',
+};
